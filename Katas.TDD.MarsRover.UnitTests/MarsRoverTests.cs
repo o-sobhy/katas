81bc7294902db6ffb255b5bd68168a2a505d4be1
omar.sobhy <omar.sobhy@talabat.com>
2025-09-07 21:18:02 +0300

    add tests for single left command received

---
+++ Katas.TDD.MarsRover.UnitTests/MarsRoverTests.cs
@@ -0,0 +1,23 @@
+namespace Katas.TDD.MarsRover.UnitTests;
+
+public class MarsRoverTests
+{
+    [Theory]
+    [InlineData("N", "W")]
+    [InlineData("W", "S")]
+    [InlineData("S", "E")]
+    [InlineData("E", "N")]
+    public void Solve_WhenSingleLeftCommandReceived_ShouldRotate_AndNotMove(string initialDirection, string expectedDirection)
+    {
+        // Arrange
+        var rover = new MarsRover(initialDirection);
+        
+        // Act
+        rover.ExecuteCommand("L");
+        
+        // Assert
+        Assert.Equal(expectedDirection, rover.Direction);
+        Assert.Equal(0, rover.X);
+        Assert.Equal(0, rover.Y);
+    }
+}
---
+++ Katas.TDD.MarsRover/MarsRover.cs
@@ -0,0 +1,9 @@
+﻿namespace Katas.TDD.MarsRover;
+
+public class MarsRover()
+{
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to Mars Rover!");
+    }
+}
