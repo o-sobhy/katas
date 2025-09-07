ced6560981ad1d333699150f5e2b80dc64c6e890
omar.sobhy <omar.sobhy@talabat.com>
2025-09-07 20:53:32 +0300

    add logic for number is a multiple of three only

--- Katas.TDD.FizzBuzz/FizzBuzzSolver.cs
+++ Katas.TDD.FizzBuzz/FizzBuzzSolver.cs
@@ -6,6 +6,8 @@ public class FizzBuzzSolver
     {
         if (number % 3 == 0 && number % 5 == 0)
             return "FizzBuzz";
+        if (number % 3 == 0)
+            return "Fizz";
         return number.ToString();
     }
     
